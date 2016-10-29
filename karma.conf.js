ba4a71da389ab4827022224828fe7366706a59a5
Ryan Ruan <ruanjiakan@gmail.com>
2016-10-30 00:34:21 +0800

    Use phantomJs

--- karma.conf.js
+++ karma.conf.js
@@ -78,8 +78,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['PhantomJS'],
+    phantomjsLauncher: {
+        // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+         exitOnResourceError: true
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
